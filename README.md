c594295e1f7751815b7ca65fce6565fba63a4bf0
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-08-17 18:57:30 -0400

    added suedo code to assignment to map out how app will work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # password_generator
+
+Changes Made:
+
+1. 
--- script.js
+++ script.js
@@ -3,12 +3,173 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = clickButton();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+var expectSpecialChars = false;
+var expectNumericChars = false;
+var expectLowerCaseChars = false;
+var expectUpperCaseChars = false;
+
+// returns a string
+function clickButton() {
+
+  //  ==============    var definitions   ==================
+
+  // Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+]; 
+
+var userOptionalChars = [];
+
+// ============= functions definitions ==================
+
+function getUserOptions () {
+   // get options form user and store in vars
+
+  // check for length (should be longer than 8 and shorter than 128)
+
+  // check for special characters
+  // check if numeric characters
+  // check if lower case characters
+  // check if upper case charaters
+
+  // check that atleast one is true
+    // else alert user
+
+  // must have one of them
+
+}
+
+function generatePassword(userOptions) {
+  var password = [];
+
+  // if lower
+    // push a random lower char to password 
+    // add lowerCharsArray to userOptionalChars
+
+  // if upper
+    // push a random upper char to password 
+    // add upperCharsArray to userOptionalChars
+
+
+  // if special
+    // push a random special char to password 
+    // add specialCharsArray to userOptionalChars
+
+
+  // if numeric
+    // push a random numeric char to password 
+    // add numericCharsArray to userOptionalChars
+
+    // for loop between start number of elements in password to the requested number of chars
+
+    // mutate the array to a string
+    // return password string
+
+  
+
+}
+
+// ============= function calls ==================
+
+// getUserOptions();
+
+// generatePassword(userOptions);
+ 
+  
+
+  
+
+
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
